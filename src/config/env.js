9a5af1f75927081a724e955653d0eb18e0922b24
dzungforex0809 <noxplayer6x@gmail.com>
2026-03-24 12:48:42 +0000

    HSK Front Final 1

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 // Config for different environments
 const config = {
   development: {
-    REACT_APP_API_BASE_URL: "http://localhost:4000/api/v2",
+    REACT_APP_API_BASE_URL: "https://backend.tiengtrung86hsk.com/api/v2",
   },
   production: {
     REACT_APP_API_BASE_URL: "https://backend.tiengtrung86hsk.com/api/v2",
@@ -9,8 +9,8 @@ const config = {
 };
 
 // Get current environment
-const env = process.env.NODE_ENV || 'development';
-const currentConfig = config[env] || config.development;
+const env = process.env.NODE_ENV || 'production';
+const currentConfig = config[env] || config.production;
 
 export const REACT_APP_API_BASE_URL = currentConfig.REACT_APP_API_BASE_URL;
 
